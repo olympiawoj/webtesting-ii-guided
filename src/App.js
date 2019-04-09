eff939adba960800dba8031d63a0f010a78505b4
olympia <owojcik7@gmail.com>
2019-04-09 13:35:41 -0400

    Write tests in class

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    greeting: "No greeting for you"
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <h2>Hello World</h2>
+        <button onClick={this.greet}>Greet</button>
+        <button onClick={this.bye}>Bye</button>
+        <div>{this.state.greeting}</div>
+      </>
     );
   }
+
+  greet = () => {
+    this.setState({ greeting: "Hello Web 17" });
+  };
+
+  bye = () => {
+    this.setState({ greeting: "Bye Web 17" });
+  };
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render, fireEvent } from "react-testing-library";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("</App>", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  //how can I write this if I were using the library instead of manually rendeirng things to DOM
+  //These are both testing the same things
+
+  it("renders succesfully", () => {
+    render(<App />);
+  });
+
+  it("renders Hello World", () => {
+    // const helpers = render(<App />);
+    // console.log("helpers", helpers);
+
+    const { getByText, queryByText } = render(<App />);
+    //use getByText to find something on page- if you use getBy and w/e you're looking for in the DOM isnt there, it will auto fail test for you.
+    //if you dont about case sensitivity or !- then make it a regex
+    // getByText(/hello world/i);
+    // queryByText(/hello world/i);
+
+    expect(queryByText(/hello world/i)).not.toBeNull();
+  });
+
+  it("greets the team", () => {
+    //render component & grba getbyText - use getbytext t find button. Click on button. Check, after I click on button, do I have Hello Web 17 in my UI. If not case, code is broken.
+    const { getByText } = render(<App />);
+    //grab the element button, now I can click on it
+    const button = getByText(/greet/i);
+
+    fireEvent.click(button);
+    getByText(/Hello Web 17/i);
+
+    const byeButton = getByText(/bye/i);
+    fireEvent.click(byeButton);
+    getByText(/Bye Web 17/i);
+  });
 });
